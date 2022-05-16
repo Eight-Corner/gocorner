5c00900dab783ba3be0a744b5df8f5ae2e35b682
corner <corner3499@gmail.com>
2022-05-17 00:52:24 +0900

    mysql DB Connect Source

--- lib/database.go
+++ lib/database.go
@@ -3,13 +3,31 @@ package lib
 import (
 	"fmt"
 	"github.com/eight-corner/learngo/constants"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"os"
 )
 
-var DB *gorm.DB
+var Db *gorm.DB
 
 func ConnectDatabase() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=False&loc=Local",
 		constants.user, constants.password, constants.host, constants.port, constants.database)
 
-	//db, err :=
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to open DB")
+	}
+
+	if os.Getenv("ENVIRONMENT") != "production" {
+
+	}
+
+	Db = db
+}
+
+func CloseDatabase() {
+	if Db != nil {
+		Db = nil
+	}
 }
