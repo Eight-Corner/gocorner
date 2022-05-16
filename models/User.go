4f68319eabcfaeb42dcffe4efacc2e44fce5651c
corner <corner3499@gmail.com>
2022-05-16 21:58:51 +0900

    models

--- models/User.go
+++ models/User.go
@@ -6,14 +6,5 @@ type User struct {
 	Username  string `json:"user_name"`
 	Password  string `json:"password"`
 	UserEmail string `json:"email"`
-}
-
-/* Response  */
-type UserResult struct {
-	No        uint64 `json:"no"`
-	Username  string `json:"user_name"`
-	UserEmail string `json:"email"`
 	CreatedAt string `json:"created_at"`
 }
-
-/* Request Body */
