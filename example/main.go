cf9659c371e644a56c082c1c2cec1a237bcc1306
corner <corner3499@gmail.com>
2022-05-17 22:49:45 +0900

    if / switch

--- example/main.go
+++ example/main.go
@@ -27,7 +27,45 @@ func superAdd(numbers ...int) int {
 	return total
 }
 
+func canIdrink(age int) bool {
+	/*
+		if koreanAge := age + 2; koreanAge < 18 {
+			return false
+		} else {
+			return true
+		}*/
+
+	/*switch {
+	case age < 18:
+		return false
+	case age == 18:
+		return true
+	case age > 50:
+		return false
+	}
+	return false
+	*/
+	/*
+		if age < 18 {
+			return false
+		} else if age == 18 {
+			return true
+		} else if age > 50 {
+			return false
+		}
+		return false*/
+
+	switch korAge := age + 2; korAge {
+	case 10:
+		return false
+	case 18:
+		return true
+	}
+	return false
+
+}
+
 func main() {
-	total := superAdd(1, 2, 3, 4, 5, 6, 7)
-	fmt.Println(total)
+	fmt.Println(canIdrink(18))
+
 }
